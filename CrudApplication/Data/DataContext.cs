3afda2e2dddc9e30efd21fa424c674c21d0a5f4b
charithroshan <charithroshangnc@gmail.com>
2022-08-05 13:36:27 +0530

    Initial commit

---
+++ CrudApplication/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudApplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server = localhost\\sqlexpress; database = CrudApplicationDb; trusted_connection = true";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ CrudApplication/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudApplication.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string IpAddres { get; set; }
+
+    }
+}
